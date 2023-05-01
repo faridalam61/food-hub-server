cae74a6b95b71586ab5bc7cd2108d55f8862e899
Farid Alam <faridalam62@gmail.com>
2023-05-01 21:41:01 +0600

    Fake json data created and routes created to access all chefs and recipies

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const chefs = require('./data/chefs.json');
+const recipies = require('./data/recipies.json');
 
 app.get('/', (req, res) => {
     res.send('Server is running..')
 })
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
 
+app.get('/recipies', (req, res) => {
+    res.send(recipies);
+})
 app.listen(port, () => {
     console.log('Server is running on the port ',port)
 })
